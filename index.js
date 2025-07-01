d45ff3757ef6425f04751e700ad89bbdc3de4172
Sushant Shinde <98010962+Maverick341@users.noreply.github.com>
2025-07-01 13:50:20 +0530

    fix: remove deprecated set-output usage

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const core = require('@actions/core');
 const https = require('https');
+const fs = require('fs');
 
 try {
   const urlToPing = core.getInput('url');
   if (!urlToPing) {
-    return core.setFailed('URL is required!');
+    core.setFailed('URL is required!');
+    return;
   }
   https
     .get(urlToPing, () => {
-      core.setOutput('✅ Success');
+      // core.setOutput('✅ Success');
+      fs.appendFileSync(process.env.GITHUB_OUTPUT, `status=🟢 Ping Success\n`)
     })
-    .on('error', (error) => core.setOutput(`🛑 ${error.message}`));
+    .on('error', (error) => {
+      // core.setOutput(`🛑 ${error.message}`);
+      fs.appendFileSync(process.env.GITHUB_OUTPUT, `status=🛑 ${error.message}\n`);
+    });
 } catch (error) {
   core.setFailed(`🛑 ${error.message}`);
 }
